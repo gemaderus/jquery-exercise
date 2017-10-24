90500ab01bb2805ab95f833e8191f3a1c2ae2322
gemaderus <hi@gemaderus.com>
2017-10-24 12:11:45 +0200

    Blur fixed.

--- demo.js
+++ demo.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   $("#menu ul li:first").addClass("selected");
-  $("section article:nth-child(2)").addClass("middle");
+  $("sectionF article:nth-child(2)").addClass("middle");
   $(".list-item:odd").addClass("highlighted");
   $(".list-item:last-of-type").css("border", "1px solid #eee");
   $("input").focus(function() {
     $(this).css("border", "1px solid #eee");
   });
 
-  // $("input").blur(
-  //   function(){
-  //       $(this).css("border-color" : "initial");
-  //   });
+  $("input").blur(function() {
+    $(this).css("border", "");
+  });
+
 });
